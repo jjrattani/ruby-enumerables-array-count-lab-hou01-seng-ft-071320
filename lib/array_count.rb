239cc88195e07cbcf7add4dc3bf7828713102096
Jessica  Rattani <jjrattani@gmail.com>
2020-07-02 16:08:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -27,7 +27,7 @@ def count_empty_strings(array)
   index = 0
   
   while index < array.length do
-    if array[index].class !== String
+    if array[index] == ""
       count_empty_strings += 1
     end
     index += 1
